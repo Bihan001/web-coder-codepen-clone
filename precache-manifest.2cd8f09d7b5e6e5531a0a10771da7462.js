3cbfad47e7a0793a46162d970653af6e9667f770
Bihan Chakraborty <bhchakraborty001@gmail.com>
2020-11-06 19:18:15 +0530

    Updates

---
+++ precache-manifest.2cd8f09d7b5e6e5531a0a10771da7462.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f2312d91e4e9efcf5bc00588f16b3c7",
+    "url": "/web-coder-codepen-clone/index.html"
+  },
+  {
+    "revision": "65b06863ff56ca341df1",
+    "url": "/web-coder-codepen-clone/static/css/2.9de32946.chunk.css"
+  },
+  {
+    "revision": "fa1a792088d76a9bf48e",
+    "url": "/web-coder-codepen-clone/static/css/main.6787aedf.chunk.css"
+  },
+  {
+    "revision": "65b06863ff56ca341df1",
+    "url": "/web-coder-codepen-clone/static/js/2.bcf18c92.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/web-coder-codepen-clone/static/js/2.bcf18c92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa1a792088d76a9bf48e",
+    "url": "/web-coder-codepen-clone/static/js/main.4df9d157.chunk.js"
+  },
+  {
+    "revision": "79278256a39ae5fffb66",
+    "url": "/web-coder-codepen-clone/static/js/runtime-main.04525c49.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/web-coder/precache-manifest.f5ce8596bd48c155c88c9439677d323c.js"
+  "/web-coder-codepen-clone/precache-manifest.2cd8f09d7b5e6e5531a0a10771da7462.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/web-coder/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/web-coder-codepen-clone/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
